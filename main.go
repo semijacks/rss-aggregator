783b9e3dac0194084d6220dc7d0219ae9e631de8
semijacks <semijacks27@gmail.com>
2024-02-10 10:56:18 +0100

    json modules and route handlers

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main () {
-	fmt.Println("Hello world")
-
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -22,10 +20,28 @@ func main () {
 
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string {"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server {
 		Handler: router,
 		Addr: ":" + portString,
 	}
+
+	log.Printf("Server is running on port %v", portString)
 
 	err := srv.ListenAndServe()
 
